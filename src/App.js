b7f426778b80df8c497914ac26406c3a7a63038b
Alexey Yudin <alexey.y.yudin@gmail.com>
2019-08-22 14:33:18 +0200

    Add a book item component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import {Component} from 'react';
 import './App.css';
+import {BookItem} from "./book-item/book-item";
 
 class App extends Component {
+  book = {
+    image: '/images/godfather.jpg',
+    title: 'Godfather',
+    text: `With its brilliant and brutal portrayal of the Corleone family,
+      The Godfather burned its way into our national consciousness.
+      This unforgettable saga of crime and corruption, passion and loyalty continues to
+      stand the test of time, as the definitive novel of the Mafia underworld.\n`
+  };
+
   render() {
     return (
-      <div>
-        Hello, React!
+      <div className="app">
+        <BookItem
+          image={this.book.image}
+          title={this.book.title}
+          text={this.book.text}
+        />
       </div>
     );
   }
